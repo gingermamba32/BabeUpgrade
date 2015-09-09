c30f31de4dcc09f97e6b9a858654c47629e1b379
Michael_Montero <mmontero2012@gmail.com>
2015-09-09 16:38:31 -0600

    got find to work locally

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,65 @@ catch(err){
 }
 console.log("uristring is "+ uristring);
 
+var db = mongoose.connect( uristring);
+
+// db schema for the locations collection
+var Locations = db.model('location', { 	
+	shipment: {
+        type: String,
+        default: '',
+        required: 'PO # HERE',
+        trim: false
+        },
+    type: {
+        type: String,
+        default: '',
+        required: 'Please Enter the UPC',
+        trim: false
+        },
+    length: {
+        type: String,
+        default: '',
+        required: 'Please Enter the UPC',
+        trim: false
+        },
+    color: {
+        type: String,
+        default: '',
+        required: 'Please Enter the UPC',
+        trim: false
+        },
+    location: {
+        type: String,
+        default: '',
+        required: 'RACK LOCATION HERE',
+        trim: false
+        },
+    quantity: {
+        type: Number,
+        default: '',
+        required: 'QTY?',
+        trim: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'bbbbbbbb' });
-});
+	Locations.find( {}, function(err, docs) {
+		docs.reverse();
+		console.log(docs + "products");
+	res.render('index', { 'products': docs });
+	});
+
+
+})
+
+
 
 module.exports = router;
